2cd7a068ebbb80f72c9370bde9e104d8f0cf262a
JohnMave126 <johnmave126@gmail.com>
2013-03-25 17:25:01 +0800

    Add skeleton of deque
    Signed-off-by: JohnMave126 <johnmave126@gmail.com>

---
+++ dequeue/deque.cpp
@@ -0,0 +1,38 @@
+/*
+ * Dequeue for COMP2012H
+ * Author: TAN, Shuhao
+ * Stu ID: 20090398
+ * 2013 Spring
+ *
+ * deque.h
+ *
+ * implementation for dequeue
+ */
+
+#include "deque.h"
+
+bool Deque::isEmpty() const {
+	return false;
+}
+
+int Deque::size() const {
+	return 0;
+}
+
+void addFirst(T item) {
+	return;
+}
+
+void addLast(T item) {
+	return;
+}
+
+T removeFirst() {
+}
+
+T removeLast() {
+}
+
+int main() {
+	return 0;
+}
---
+++ dequeue/deque.h
@@ -0,0 +1,103 @@
+/*
+ * Dequeue for COMP2012H
+ * Author: TAN, Shuhao
+ * Stu ID: 20090398
+ * 2013 Spring
+ *
+ * deque.h
+ *
+ * header file for dequeue
+ */
+
+#ifndef _DEQUE_H
+#define _DEQUE_H
+
+//Declaration of dequeue
+template<class T>
+class Deque {
+	private:
+		//Type definition
+		//Node for double linked list
+		struct node {
+			//Store the data
+			T *data;
+			//Previous and Next node
+			node *prev;
+			node *next;
+			//Reference count
+			int cnt;
+			//Detached flag
+			bool valid;
+		};
+	
+	public:
+		/* default constructor/destructor */
+		Deque();
+		~Deque();
+		
+		/*
+		 * isEmpty
+		 *
+		 * return true if the dequeue is empty
+		 */
+		bool isEmpty() const;
+		
+		/*
+		 * size
+		 *
+		 * return the size of the dequeue
+		 */
+		int size() const;
+		
+		/*
+		 * addFirst
+		 *
+		 * item: the item to add
+		 *
+		 * insert the item at the front of the dequeue
+		 */
+		void addFirst(T item);
+		
+		/*
+		 * addLast
+		 *
+		 * item: the item to add
+		 *
+		 * insert the item at the end of the dequeue
+		 */
+		void addLast(T item);
+		
+		/*
+		 * removeFirst
+		 *
+		 * delete and return the first item in dequeue
+		 */
+		T removeFirst();
+		
+		/*
+		 * removeLast
+		 *
+		 * delete and return the last item in dequeue
+		 */
+		T removeLast();
+		
+		//Iterator class
+		class Iterator {
+			public:
+				
+			
+			private:
+				//Pointer to element
+				node *elem;
+		};
+		//Give permission to Iterator
+		friend class Iterator;
+		
+		//The pseudo end
+		const node *end;
+		
+		//The pointer to head
+		node *head;
+};
+
+#endif
